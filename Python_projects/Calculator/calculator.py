161575f64e874f870a8774b9858c3b06cbf1e9b1
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-28 18:31:39 +0200

    Created Calculator

---
+++ Python_projects/Calculator/calculator.py
@@ -0,0 +1,27 @@
+def main():
+    print("--- Dictionary Calculator ---")
+
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    results = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2
+    }
+
+    if op == "/":
+        if num2 == 0:
+            print("Error: Division by zero.")
+        else:
+            print("Result:", num1 / num2)
+
+    elif op in results:
+        print("Result:", results[op])
+
+    else:
+        print("Error: Invalid operator.")
+
+
+
